cc06150cb6016f975b0a59ffa9e84f329462d108
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2020-09-11 17:21:57 +0100

    Add setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='rdbl',
     version='0.1.0',
@@ -7,9 +10,9 @@ setup(
     author_email='nikhil.woodruff@outlook.com',
     packages=['rdbl'],
     scripts=[],
-    url='',
+    url='https://github.com/nikhilwoodruff/rdbl',
     license='',
     description='A lightweight package to format numbers for readability.',
-    long_description='',
+    long_description=long_description,
     install_requires=[],
 )
